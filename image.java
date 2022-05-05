9c2a498e89e4ac79344d26a32f7e065b665ec9d3
BEYZA <44142273+beyzkrmn@users.noreply.github.com>
2022-05-05 11:12:10 +0300

    Add files via upload

---
+++ image.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospitalmanagementsystem;
+
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Beyza
+ */
+public class image {
+    
+    //ImageIcon icon = new ImageIcon ("C:\Users\Beyza\Downloads\Exit.png");
+    
+}
